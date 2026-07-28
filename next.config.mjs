dd8d0e0d993d38805a7fe6fadb8e3fb0eb687708
Suzana Zatorre <suzanazatore@suzanazatorre.com.br>
2026-07-28 01:40:26 -0300

    Libera server actions no Codespace

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: ['*.app.github.dev']
+    }
+  },
   images: {
     remotePatterns: [
       {
